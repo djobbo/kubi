e7a81b2bea23c5e56c1e2e36e226baea475a19cd
Djobbo <hi@djobbo.com>
2025-07-10 00:47:36 +0200

    remove hono routes

--- packages/effect-ts/src/index.ts
+++ packages/effect-ts/src/index.ts
@@ -1,5 +1,25 @@
+import { ApiLive } from './api'
+import { HttpApiBuilder, HttpApiSwagger, HttpServer } from "@effect/platform"
+import { BunHttpServer, BunRuntime } from "@effect/platform-bun"
 import { Layer } from "effect"
-import * as HonoApi from "./routes"
-import { Runtime } from "./runtime"
+import * as Archive from "./services/archive"
+import * as BrawlhallaApi from "./services/brawlhalla-api"
 
-Runtime.runFork(Layer.launch(HonoApi.fromEnv))
+import * as Fetcher from "./helpers/fetcher"
+import * as DB from "./services/db"
+
+const ServerLive = HttpApiBuilder.serve().pipe(
+	Layer.provide(HttpApiBuilder.middlewareCors({
+		allowedOrigins: [],
+	})),
+	Layer.provide(HttpApiSwagger.layer()),
+	HttpServer.withLogAddress,
+	Layer.provide(ApiLive),
+	Layer.provide(BunHttpServer.layer({ port: 3000, hostname: "0.0.0.0" })),
+	Layer.provide(BrawlhallaApi.fromEnv),
+	Layer.provide(Archive.layer()),
+	Layer.provide(Fetcher.fromEnv),
+	Layer.provide(DB.fromEnv),
+)
+
+Layer.launch(ServerLive).pipe(BunRuntime.runMain)
