a793e32e827407c65aad86c4225d1010c031a505
Djobbo <hi@djobbo.com>
2025-11-27 17:41:03 +0100

    fix guild level calculation

--- packages/brawlhalla-api/src/constants/guilds.ts
+++ packages/brawlhalla-api/src/constants/guilds.ts
@@ -1,12 +1,20 @@
+import { clamp } from "@dair/common/src/helpers/math"
+
 export const guildLevels = [0, 20_000, 200_000, 800_000, 2_000_000]
 
-export const getGuildLevel = (xp: number) => {
-  const level = guildLevels.findIndex((level) => xp >= level) + 1
-  const levelXp = guildLevels[level]
-  if (!levelXp) {
+export const getGuildLevel = (guildXp: number) => {
+  const level = guildLevels.findIndex((requiredXp) => requiredXp >= guildXp)
+  const currentLevelRequiredXp = guildLevels[level - 1]
+  const nextLevelRequiredXp = guildLevels[level]
+  if (!currentLevelRequiredXp || !nextLevelRequiredXp) {
     return { level: guildLevels.length, xpPercentage: 100 }
   }
 
-  const xpPercentage = Math.min(100, ((xp - levelXp) / levelXp) * 100)
+  const xpPercentage = clamp(
+    (guildXp - currentLevelRequiredXp) /
+      (nextLevelRequiredXp - currentLevelRequiredXp),
+    0,
+    1,
+  )
   return { level, xpPercentage }
 }
