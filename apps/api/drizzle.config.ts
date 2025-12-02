e64e5e7d5769bca3b25ad736d152f4a4dc7941df
Djobbo <hi@djobbo.com>
2025-12-02 14:43:10 +0100

    apps folder

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "drizzle-kit"
 
 export default defineConfig({
-  schema: "../packages/schema/src/index.ts",
+  schema: "../../packages/schema/src/index.ts",
   out: "./migrations",
   dialect: "sqlite",
   dbCredentials: { url: process.env.DATABASE_URL! },
