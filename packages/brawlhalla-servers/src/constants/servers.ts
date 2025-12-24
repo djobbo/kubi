6659d04dce82bbb3bc024113bf6251b32ed97df3
Djobbo <hi@djobbo.com>
2025-12-24 02:14:53 +0100

    change build pipeline

--- packages/brawlhalla-servers/src/constants/servers.ts
+++ packages/brawlhalla-servers/src/constants/servers.ts
@@ -1,110 +1,110 @@
 export const servers = [
   {
-    id: "US-E",
-    url: "pingtest-atl.brawlhalla.com",
-    ip: "99.78.180.158",
-    location: {
-      city: "Dublin",
-      country: "United States",
-      lat: 40.0992,
-      lon: -83.1141,
-      isp: "Amazon.com, Inc.",
-    },
+    "id": "US-E",
+    "url": "pingtest-atl.brawlhalla.com",
+    "ip": "99.78.178.221",
+    "location": {
+      "city": "Dublin",
+      "country": "United States",
+      "lat": 40.0992,
+      "lon": -83.1141,
+      "isp": "Amazon.com, Inc."
+    }
   },
   {
-    id: "US-W",
-    url: "pingtest-cal.brawlhalla.com",
-    ip: "176.32.118.39",
-    location: {
-      city: "San Jose",
-      country: "United States",
-      lat: 37.3394,
-      lon: -121.895,
-      isp: "Amazon.com, Inc.",
-    },
+    "id": "US-W",
+    "url": "pingtest-cal.brawlhalla.com",
+    "ip": "176.32.118.39",
+    "location": {
+      "city": "San Jose",
+      "country": "United States",
+      "lat": 37.3394,
+      "lon": -121.895,
+      "isp": "Amazon.com, Inc."
+    }
   },
   {
-    id: "EU",
-    url: "pingtest-ams.brawlhalla.com",
-    ip: "52.94.137.29",
-    location: {
-      city: "Frankfurt am Main",
-      country: "Germany",
-      lat: 50.1109,
-      lon: 8.68213,
-      isp: "Amazon.com, Inc.",
-    },
+    "id": "EU",
+    "url": "pingtest-ams.brawlhalla.com",
+    "ip": "52.94.141.11",
+    "location": {
+      "city": "Frankfurt am Main",
+      "country": "Germany",
+      "lat": 50.1109,
+      "lon": 8.68213,
+      "isp": "Amazon.com, Inc."
+    }
   },
   {
-    id: "SEA",
-    url: "pingtest-sgp.brawlhalla.com",
-    ip: "15.221.8.222",
-    location: {
-      city: "Singapore",
-      country: "Singapore",
-      lat: 1.28009,
-      lon: 103.851,
-      isp: "Amazon.com, Inc.",
-    },
+    "id": "SEA",
+    "url": "pingtest-sgp.brawlhalla.com",
+    "ip": "15.221.8.219",
+    "location": {
+      "city": "Singapore",
+      "country": "Singapore",
+      "lat": 1.28009,
+      "lon": 103.851,
+      "isp": "Amazon.com, Inc."
+    }
   },
   {
-    id: "AUS",
-    url: "pingtest-aus.brawlhalla.com",
-    ip: "3.27.178.188",
-    location: {
-      city: "Sydney",
-      country: "Australia",
-      lat: -33.8591,
-      lon: 151.2002,
-      isp: "Amazon Technologies Inc.",
-    },
+    "id": "AUS",
+    "url": "pingtest-aus.brawlhalla.com",
+    "ip": "99.83.80.154",
+    "location": {
+      "city": "Sydney",
+      "country": "Australia",
+      "lat": -33.8591,
+      "lon": 151.2002,
+      "isp": "Amazon.com, Inc."
+    }
   },
   {
-    id: "BRZ",
-    url: "pingtest-brs.brawlhalla.com",
-    ip: "177.72.245.165",
-    location: {
-      city: "São Paulo",
-      country: "Brazil",
-      lat: -23.5505,
-      lon: -46.6333,
-      isp: "Amazon.com, Inc.",
-    },
+    "id": "BRZ",
+    "url": "pingtest-brs.brawlhalla.com",
+    "ip": "177.72.245.165",
+    "location": {
+      "city": "São Paulo",
+      "country": "Brazil",
+      "lat": -23.5505,
+      "lon": -46.6333,
+      "isp": "Amazon.com, Inc."
+    }
   },
   {
-    id: "JPN",
-    url: "pingtest-jpn.brawlhalla.com",
-    ip: "99.77.58.84",
-    location: {
-      city: "Tokyo",
-      country: "Japan",
-      lat: 35.6895,
-      lon: 139.692,
-      isp: "Amazon.com",
-    },
+    "id": "JPN",
+    "url": "pingtest-jpn.brawlhalla.com",
+    "ip": "99.77.60.112",
+    "location": {
+      "city": "Tokyo",
+      "country": "Japan",
+      "lat": 35.6895,
+      "lon": 139.692,
+      "isp": "Amazon.com"
+    }
   },
   {
-    id: "ME",
-    url: "pingtest-mde.brawlhalla.com",
-    ip: "99.82.136.87",
-    location: {
-      city: "Manama",
-      country: "Bahrain",
-      lat: 26.2167,
-      lon: 50.5833,
-      isp: "Amazon.com, Inc.",
-    },
+    "id": "ME",
+    "url": "pingtest-mde.brawlhalla.com",
+    "ip": "99.82.128.102",
+    "location": {
+      "city": "Manama",
+      "country": "Bahrain",
+      "lat": 26.2167,
+      "lon": 50.5833,
+      "isp": "Amazon.com, Inc."
+    }
   },
   {
-    id: "SA",
-    url: "pingtest-saf.brawlhalla.com",
-    ip: "99.78.132.94",
-    location: {
-      city: "Johannesburg",
-      country: "South Africa",
-      lat: -26.2041,
-      lon: 28.0473,
-      isp: "Amazon.com, Inc.",
-    },
-  },
+    "id": "SA",
+    "url": "pingtest-saf.brawlhalla.com",
+    "ip": "99.78.132.94",
+    "location": {
+      "city": "Johannesburg",
+      "country": "South Africa",
+      "lat": -26.2041,
+      "lon": 28.0473,
+      "isp": "Amazon.com, Inc."
+    }
+  }
 ] as const
