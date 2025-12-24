72cd779585eb30e5d900b99ec4a3176888023fa5
Djobbo <hi@djobbo.com>
2025-12-24 02:40:35 +0100

    yikers

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "drizzle-kit"
 
 const DATABASE_URL = process.env.DATABASE_URL
 
+console.log("env", process.env)
+
 if (!DATABASE_URL) {
   throw new Error("DATABASE_URL is not set")
 }
