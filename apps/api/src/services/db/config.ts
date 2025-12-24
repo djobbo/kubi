3060a93d89bd4e8ab351bcf5c2df7d31ed121537
Djobbo <hi@djobbo.com>
2025-12-24 02:04:14 +0100

    remove cacheversion

--- apps/api/src/services/db/config.ts
+++ apps/api/src/services/db/config.ts
@@ -7,16 +7,14 @@ export class DatabaseConfig extends Context.Tag("@app/DatabaseConfig")<
   DatabaseConfig,
   {
     readonly url: Redacted.Redacted
-    readonly cacheVersion: number
   }
 >() {
   static readonly layer = Layer.effect(
     DatabaseConfig,
     Effect.gen(function* () {
       const url = yield* Config.nonEmptyString("DATABASE_URL")
-      const cacheVersion = yield* Config.number("DATABASE_CACHE_VERSION")
 
-      return DatabaseConfig.of({ url: Redacted.make(url), cacheVersion })
+      return DatabaseConfig.of({ url: Redacted.make(url) })
     }),
   )
 }
