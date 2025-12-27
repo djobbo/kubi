629dd06b8e03893b022c003fbae38fd0582fcef0
Djobbo <hi@djobbo.com>
2025-12-27 13:36:24 +0100

    monitoring stack compose

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from "drizzle-kit"
 
 const DATABASE_URL = process.env.DATABASE_URL
 
-console.log("env", process.env)
-
 if (!DATABASE_URL) {
   throw new Error("DATABASE_URL is not set")
 }
